ba962b2a69b29cbee0218ed7ee9a3e55bc46640c
Matt Hibbs <mhibbs@trinity.edu>
2022-03-21 14:10:46 -0500

    Finished word counts

--- src/main/scala/cs2/algorithms/WordCount.scala
+++ src/main/scala/cs2/algorithms/WordCount.scala
@@ -9,14 +9,35 @@ object WordCount {
     val path = getClass().getResource("/tempest.txt")
     val lines = Source.fromURL(path).getLines()
 
+    /*
     val wordSet = scala.collection.mutable.Set[String]()
-
     for(line <- lines) {
       val words = line.split("\\s+").map(_.filter(_.isLetter).toLowerCase())
       wordSet ++= words
     }
     for(word <- wordSet) println(word)
     println(wordSet.size)
+    */
+
+    val countMap = scala.collection.mutable.Map[String,Int]()
+    for(line <- lines) {
+      val words = line.split("\\s+").map(_.filter(_.isLetter).toLowerCase())
+      for(word <- words) {
+        if(countMap.contains(word)) countMap(word) += 1
+        else countMap(word) = 1
+      }
+    }
+    countMap -= ""
+
+    var bigKey:String = countMap.keys.toArray.apply(0)
+    for(tup <- countMap) {
+      if(tup._2 > countMap(bigKey)) bigKey = tup._1
+    }
+    println(bigKey + " --> " + countMap(bigKey))
+
+    val arr = countMap.toArray
+    ParametricSorting.bubbleSort(arr, (x:(String,Int), y:(String,Int)) => x._2 > y._2)
+    println(arr.mkString("\n"))
 
   }
 
